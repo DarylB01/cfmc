07978f529da8c38994ec5fd987441fe7776facbb
Gladzy Kei Zuniga <keikei@Gladzys-MBP.hitronhub.home>
2020-04-20 12:51:16 -0600

    connected to backend and hid api keys

---
+++ build/precache-manifest.8ac0021d976df940e7aeb010ac71d3b3.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02c4dfadb3f233b24bbf86508e51994d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8386d1ffa8dfddcf79c",
+    "url": "/static/css/2.264cb77e.chunk.css"
+  },
+  {
+    "revision": "73a7ba07d32c001bab1a",
+    "url": "/static/css/main.ea9e1368.chunk.css"
+  },
+  {
+    "revision": "e8386d1ffa8dfddcf79c",
+    "url": "/static/js/2.d5afb5e5.chunk.js"
+  },
+  {
+    "revision": "a19803e00822c83401db1fe4a7125968",
+    "url": "/static/js/2.d5afb5e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73a7ba07d32c001bab1a",
+    "url": "/static/js/main.37aedaab.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "6fb7bda81f689f17e11024c688041268",
+    "url": "/static/media/about-bg.6fb7bda8.jpg"
+  },
+  {
+    "revision": "107ef9cdd6bc6c31cd2e2422447344bf",
+    "url": "/static/media/banner-img.107ef9cd.jpg"
+  },
+  {
+    "revision": "8ac90699832ef8dcafaadf0397391fba",
+    "url": "/static/media/banner-img.8ac90699.jpg"
+  },
+  {
+    "revision": "c3c9314ebc3a832b63192c933046b34d",
+    "url": "/static/media/cfmc-icon.c3c9314e.jpg"
+  },
+  {
+    "revision": "89f93b6b009117bc1af47cadc4257b9c",
+    "url": "/static/media/cfmcLogo.89f93b6b.png"
+  },
+  {
+    "revision": "256e6615a48ba8f814703caefc723d2e",
+    "url": "/static/media/cfmcLogoWhite.256e6615.png"
+  },
+  {
+    "revision": "6cdbcbafdc443919a90348e6f0b3018e",
+    "url": "/static/media/church-bg.6cdbcbaf.jpg"
+  },
+  {
+    "revision": "7a6f5aed41dc727661517e7c58fd5067",
+    "url": "/static/media/contact-bg.7a6f5aed.jpg"
+  },
+  {
+    "revision": "0a8f74b30316489128f58bd247302795",
+    "url": "/static/media/filler-event.0a8f74b3.jpg"
+  },
+  {
+    "revision": "5489f27cb1d7a96a74c4a457530cd724",
+    "url": "/static/media/filler-img.5489f27c.jpg"
+  },
+  {
+    "revision": "316719bc1cee8fffb0053a93896238cd",
+    "url": "/static/media/footer-bg.316719bc.jpg"
+  },
+  {
+    "revision": "3ccb8c33407f1d5ee225f8beccfb3308",
+    "url": "/static/media/gallery (1).3ccb8c33.jpg"
+  },
+  {
+    "revision": "f7c9f88266780d61942ee5529c785840",
+    "url": "/static/media/gallery (2).f7c9f882.jpg"
+  },
+  {
+    "revision": "b181b24c0a2fd47fdf7df232e3116559",
+    "url": "/static/media/gallery (3).b181b24c.jpg"
+  },
+  {
+    "revision": "0711de15db93ce4005d031f798599515",
+    "url": "/static/media/img1.0711de15.jpg"
+  },
+  {
+    "revision": "8acfd424a86abea117a88c81ae63a609",
+    "url": "/static/media/img2.8acfd424.jpg"
+  },
+  {
+    "revision": "938c755c5a5bd3601a005e8f0028a581",
+    "url": "/static/media/img3.938c755c.jpg"
+  },
+  {
+    "revision": "f20054f927ee871c2f43228834a0b1da",
+    "url": "/static/media/img4.f20054f9.jpg"
+  },
+  {
+    "revision": "d378f42fbf26fe440da9b736b66a862d",
+    "url": "/static/media/live-bg.d378f42f.jpg"
+  },
+  {
+    "revision": "51d58e3cba690bd4a5a9d12cd51cf344",
+    "url": "/static/media/liveImg.51d58e3c.jpg"
+  },
+  {
+    "revision": "215327b65d17c48defd81c03488a23ad",
+    "url": "/static/media/preview.215327b6.jpg"
+  }
+]);
